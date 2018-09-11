14390984921011d5cb8f8f996d735a97a0c18ae6
Eddy Izakson <eddyizakson@gmail.com>
2018-09-11 18:07:26 +0300

    Created ReplaySystem.cs & GameManager.cs
    Note: Input ("Fire1") not working properly, check original scripts and try again.
    Input "Fire1" not leaving Recording mode while pressed but the functionality works.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour
+{
+    public bool recording = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        recording = !CrossPlatformInputManager.GetButton("Fire1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
